af263a861cd868fced903b8cfb9f6f10850c70de
chenweizhe <986523239@qq.com>
2017-05-04 19:16:47 +0800

    repair adapter

--- app/src/main/java/com/example/strivezhe_chen/footballfans/Adapter/sportnewsAdapter.java
+++ app/src/main/java/com/example/strivezhe_chen/footballfans/Adapter/sportnewsAdapter.java
@@ -54,7 +54,7 @@ public class sportnewsAdapter extends BaseAdapter implements AdapterView.OnItemC
         }
 
         firstIn = true;
-        
+
         sportListView.setOnItemClickListener(this);
     }
 
